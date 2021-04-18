88f4bbf79be6621b46f6d1472072002021025294
Dan Phiffer <dan@phiffer.org>
2021-04-18 11:19:30 -0400

    lots and lots and lots

---
+++ wp/wp-content/themes/lawnlab/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+add_filter('acf/settings/load_json', function($path) {
+	return get_stylesheet_directory() . '/acf';
+});
+
+add_filter('acf/settings/save_json', function($path) {
+	return get_stylesheet_directory() . '/acf';
+});
+
+function asset_url($file) {
+	echo get_asset_url($file);
+}
+
+function get_asset_url($file, $return_version = false) {
+	$url  = get_stylesheet_directory_uri() . "/$file";
+	$path = get_stylesheet_directory() . "/$file";
+	$ver  = filemtime($path);
+	if ($return_version) {
+		return [$url, $ver];
+	} else {
+		return "$url?$ver";
+	}
+}
---
+++ wp/wp-content/themes/lawnlab/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" href="<?php asset_url('dist/main.css'); ?>">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<div class="container">
+			<div id="content" class="main">
