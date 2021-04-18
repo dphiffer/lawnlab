4013053fa8dd33941759e665180870e2a3030247
Dan Phiffer <dan@phiffer.org>
2021-04-18 16:04:11 -0400

    home intro

---
+++ wp/wp-content/themes/lawnlab/home-intro.php
@@ -0,0 +1,12 @@
+<div class="intro">
+	<div class="breadcrumbs">
+		<a href="/">Next Epoch Seed Library</a>
+	</div>
+	<video loop autoplay>
+		<source src="<?php asset_url('img/pyramid.webm'); ?>"  type="video/webm">
+	</video>
+	<div class="intro__title">
+		<h2><?php the_title(); ?></h2>
+		<h3>A collaboration with seeds, time, and weeds</h3>
+	</div>
+</div>
