ba5227b5660152064c29d5241592e33f410c80d4
Dan Phiffer <dan@phiffer.org>
2021-04-18 22:47:29 -0400

    add poster image

--- wp/wp-content/themes/lawnlab/home-intro.php
+++ wp/wp-content/themes/lawnlab/home-intro.php
@@ -2,8 +2,9 @@
 	<div class="breadcrumbs">
 		<a href="/">Next Epoch Seed Library</a>
 	</div>
-	<video loop autoplay>
+	<video poster="<?php asset_url('img/pyramid.jpg'); ?>" loop autoplay>
 		<source src="<?php asset_url('img/pyramid.webm'); ?>"  type="video/webm">
+		<source src="<?php asset_url('img/pyramid.mp4'); ?>"  type="video/mp4">
 	</video>
 	<div class="intro__title">
 		<h2><?php the_title(); ?></h2>
